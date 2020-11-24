5f623fb29ab269fec64eea96a7b09eaddef2c78a
Igor Aguiar <desenvigor@gmail.com>
2020-11-23 23:51:46 -0300

    ajustes de interface e usabilidade

--- src/main/java/webdvan/controller/GeralController.java
+++ src/main/java/webdvan/controller/GeralController.java
@@ -123,5 +123,6 @@ public class GeralController {
 
 		return mv;
 	}
+	
 
 }
--- src/main/java/webdvan/controller/UsuarioController.java
+++ src/main/java/webdvan/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package webdvan.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import webdvan.dto.BuscaEntrega;
 import webdvan.dto.RequisicaoEntrega;
+import webdvan.models.Cidades;
 import webdvan.models.Endereco;
 import webdvan.models.Entrega;
 import webdvan.models.Rota;
 import webdvan.models.Usuario;
+import webdvan.repository.CidadesRepository;
 import webdvan.repository.EnderecoRepository;
 import webdvan.repository.EntregaRepository;
 import webdvan.repository.RotaRepository;
@@ -41,6 +45,14 @@ public class UsuarioController {
 	
 	@Autowired
 	HttpSession session;
+	
+	@Autowired
+	CidadesRepository cd;
+	
+	public ArrayList<String> cidades;
+
+	BuscaEntrega buscaEntrega;
+	public ArrayList<BuscaEntrega> buscaEntregaLista;
 	
 	@RequestMapping(value ="/usuario", method = RequestMethod.POST)
 	public String cadastrar(Usuario usuario , HttpSession session ) {
@@ -101,6 +113,7 @@ public class UsuarioController {
 		List<Endereco> enderecos = reqEntrega.toEndereco();
 		
 		Usuario	user = (Usuario) session.getAttribute("usuario");
+		session.removeAttribute("buscaEntregaLista");
 
 		for (Endereco endereco : enderecos) {
 			endereco.setIdAssociado(user.getId());
@@ -132,6 +145,49 @@ public class UsuarioController {
 		entrega.setStatus(5);
 		entregaRepository.save(entrega);
 		return "redirect:/MenuCliente";
+	}
+	
+	@GetMapping(value = "/novaEncomenda")
+	public ModelAndView pesquisarRotaEntrega() {
+		ModelAndView mv = new ModelAndView("cliente/formNovoPedido.html");
+		cidades = new ArrayList<String>();
+		for (Cidades c : cd.findAll()) {
+			cidades.add(c.getCidade());
+		}
+		mv.addObject("cidades", cidades);
+
+		return mv;
+	}
+	
+	@RequestMapping(value = "/pesquisarRotasEntrega", method = RequestMethod.GET)
+	public ModelAndView pesquisarRotasEntrega(String consulta, HttpSession session) {
+
+		ModelAndView mv = new ModelAndView("redirect:/novaEncomenda");
+
+		List<Rota> rotas = rr.findByConsultaContaining(consulta);
+		buscaEntregaLista = new ArrayList<>();
+
+		for (Rota r : rotas) {
+
+			Usuario us = ur.findById(rotas.get(0).getIdUsuario()).get();
+			buscaEntrega = new BuscaEntrega();
+			buscaEntrega.setContatoAssociado(us.getTelefone());
+			buscaEntrega.setConsulta(r.getConsulta());
+			buscaEntrega.setNomeAssociado(us.getEmail());
+			buscaEntrega.setIdRota(r.getId());
+			buscaEntregaLista.add(buscaEntrega);
+		}
+
+		session.setAttribute("buscaEntregaLista", buscaEntregaLista);
+
+		return mv;
+	}
+	
+	@RequestMapping(value = "/formularioRota?{idRota}", method = RequestMethod.GET)
+	public String formularioRota(@PathVariable Long idRota, HttpSession session) {
+		Rota rota = rr.findById(idRota).get();
+		session.setAttribute("rota", rota);
+		return "redirect:/pedidoEntrega";
 	}
 		
 }
