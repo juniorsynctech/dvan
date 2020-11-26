c6926ad22e5fe44256d6e546daa2a6b402349863
Igor Aguiar <desenvigor@gmail.com>
2020-11-26 11:21:19 -0300

    Remoção de lista de pesquisa

--- src/main/java/webdvan/controller/GeralController.java
+++ src/main/java/webdvan/controller/GeralController.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -123,6 +125,12 @@ public class GeralController {
 
 		return mv;
 	}
-	
+
+	@PostMapping(value = "/limparLista")
+	public ModelAndView limparLista(HttpSession session) {
+		session.removeAttribute("buscaEntregaLista");
+		ModelAndView mv = new ModelAndView("redirect:/");
+		return mv;
+	}
 
 }
